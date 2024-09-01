066913672844bb5adc38527fe581f92cc1f863b9
Josecarlos Vidal <Josecarlosvidal2@gmail.com>
2024-09-01 19:28:31 -0400

    Soporte para celulares como iphone SE

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,28 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			gridTemplateAreas: {
+				'layout-md': [
+					'image status',
+					'image consola',
+				],
+			},
+		},
 	},
-	plugins: [],
-}
+	plugins: [
+		function ({ addUtilities }) {
+			addUtilities({
+				'.grid-template-areas-md': {
+					'grid-template-areas': `
+						'image status'
+						'image consola'
+					`, 'grid-template-rows': '40px auto',
+					'grid-template-columns': '60% auto',
+					'column-gap': '10px',
+					'row-gap': '5px',
+				},
+			}, ['responsive']);
+		},
+	],
+};
