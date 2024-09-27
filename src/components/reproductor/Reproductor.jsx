f5f4b5691205b7d12cd9d899b5bd4a900f464d62
Josecarlos Vidal <Josecarlosvidal2@gmail.com>
2024-09-27 16:25:27 -0300

    Add timer and fix end song invert thumbnail

--- src/components/reproductor/Reproductor.jsx
+++ src/components/reproductor/Reproductor.jsx
@@ -1,8 +1,10 @@
-import { useState, useRef } from 'react';
+import { useState, useRef, useEffect } from 'react';
 import Thumbnail from './Thumbnail';
 
 const Reproductor = ({ children }) => {
     const [isPlaying, setIsPlaying] = useState(false);
+    const [currentTime, setCurrentTime] = useState(0);
+    const [isEnd, setIsEnd] = useState(true);
     const audioRef = useRef(null);
 
     const handleThumbnailClick = () => {
@@ -10,21 +12,56 @@ const Reproductor = ({ children }) => {
         if (audioRef.current) {
             if (!isPlaying) {
                 audioRef.current.play();
+                setIsEnd(false);
             } else {
                 audioRef.current.pause();
             }
         }
     };
 
+    const handleTimeUpdate = () => {
+        if (audioRef.current) {
+            setCurrentTime(audioRef.current.currentTime);
+        }
+    };
+
+    const handleAudioEnd = () => {
+        setIsPlaying(false);
+        setIsEnd(true);
+    };
+
     const audioUrl = 'music/01 Wait a Minute.mp3';
+    useEffect(() => {
+        if (audioRef.current) {
+            audioRef.current.addEventListener('timeupdate', handleTimeUpdate);
+            audioRef.current.addEventListener('ended', handleAudioEnd);
+        }
+        return () => {
+            if (audioRef.current) {
+                audioRef.current.removeEventListener('timeupdate', handleTimeUpdate);
+                audioRef.current.removeEventListener('ended', handleAudioEnd);
+            }
+        };
+    }, []);
 
     return (
         <>
-            <Thumbnail onClick={handleThumbnailClick} isPlaying={isPlaying}>
+            <Thumbnail
+                onClick={handleThumbnailClick}
+                isPlaying={isPlaying}
+                isEnd={isEnd}  // Pasar el estado isEnd al Thumbnail
+                timer={currentTime.toFixed(0)}
+            >
                 {children}
             </Thumbnail>
             {audioUrl && (
-                <audio ref={audioRef} src={audioUrl} className="hidden" preload="auto" />
+                <audio
+                    id="audio-rep"
+                    ref={audioRef}
+                    src={audioUrl}
+                    className="hidden"
+                    preload="auto"
+                />
             )}
         </>
     );
--- src/components/reproductor/Thumbnail.jsx
+++ src/components/reproductor/Thumbnail.jsx
@@ -1,9 +1,10 @@
 import "../../styles/imagenStyles.css";
 
-const Thumbnail = ({ onClick, isPlaying, children }) => {
+const Thumbnail = ({ onClick, isPlaying, isEnd, timer, children }) => {
     return (
-        <div className={`imagen ${isPlaying ? 'invert-color' : ''}`} onClick={onClick}>
+        <div className={`imagen flex ${isPlaying ? 'invert-color' : ''}`} onClick={onClick}>
             {children}
+            <span className={`ml-auto mr-[5px] ${isEnd ? 'hidden' : ''}`} >{timer} s</span>
         </div>
     );
 };
