e4e4fdc90165ef47360e09d4dccd3434fefc032e
Josecarlos Vidal <Josecarlosvidal2@gmail.com>
2024-09-27 19:17:20 -0300

    Mod title for pc

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,29 +1,30 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {
-			gridTemplateAreas: {
-				'layout-md': [
-					'image status',
-					'image consola',
-				],
-			},
-		},
-	},
-	plugins: [
-		function ({ addUtilities }) {
-			addUtilities({
-				'.grid-template-areas-md': {
-					'grid-template-areas': `
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      gridTemplateAreas: {
+        "layout-md": ["image status", "image consola"],
+      },
+    },
+  },
+  plugins: [
+    function ({ addUtilities }) {
+      addUtilities(
+        {
+          ".grid-template-areas-md": {
+            "grid-template-areas": `
 						'image status'
 						'image consola'
-					`, 'grid-template-rows': '40px auto',
-					'grid-template-columns': '60% auto',
-					'column-gap': '10px',
-					'row-gap': '5px',
-				},
-			}, ['responsive']);
-		},
-	],
+					`,
+            "grid-template-rows": "40px auto",
+            "grid-template-columns": "40% auto",
+            "column-gap": "10px",
+            "row-gap": "5px",
+          },
+        },
+        ["responsive"]
+      );
+    },
+  ],
 };
