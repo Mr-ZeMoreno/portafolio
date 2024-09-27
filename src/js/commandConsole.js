1b385bfca9531ca05be30282cd15c5197736ae89
Josecarlos Vidal <Josecarlosvidal2@gmail.com>
2024-09-27 17:04:22 -0300

    Refactor

---
+++ src/js/commandConsole.js
@@ -0,0 +1,53 @@
+export class Console {
+    constructor(padre) {
+        this.padre = padre;
+    }
+
+    appendChild(inputValue, clase) {
+        const $msgLine = document.createElement("div");
+
+        $msgLine.classList.add(clase);
+        $msgLine.innerHTML = `<span class="green">></span><p></p>`;
+
+        const $parrafo = $msgLine.querySelector("p");
+        if ($parrafo) {
+            $parrafo.textContent = inputValue;
+        }
+
+        if (this.padre) {
+            this.padre.appendChild($msgLine);
+        }
+    }
+
+    command(htmlElement, inputValue) {
+        const comando = {
+            CLEAR: "clear",
+            HOLA: "hola",
+            PLAY: "player play",
+            PAUSE: "player pause",
+        }
+
+        let value = inputValue.toLowerCase();
+        if (value === comando.CLEAR) {
+            htmlElement.innerHTML = "";
+            return;
+        } else if (value === comando.HOLA) {
+            this.appendChild("Hola, ¿Cómo estás?", "alerta-msg");
+        } else if (value === comando.PLAY){
+            this.appendChild("Reproduciendo", "alerta-msg");
+            
+        }
+    }
+
+    removeAllElements(htmlElement) {
+        if (htmlElement.length >= 0) {
+            htmlElement.forEach((elemento) => {
+                elemento.remove();
+            });
+        }
+    }
+
+    limpiarInput(htmlElement) {
+        htmlElement.value = "";
+    }
+}
