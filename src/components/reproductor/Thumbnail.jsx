c164e481120820979a8cf688a79c0f305d71802a
Josecarlos Vidal <Josecarlosvidal2@gmail.com>
2024-09-27 22:53:06 -0300

    change div for button in thumbnail

--- src/components/reproductor/Thumbnail.jsx
+++ src/components/reproductor/Thumbnail.jsx
@@ -2,10 +2,10 @@ import "../../styles/imagenStyles.css";
 
 const Thumbnail = ({ onClick, isPlaying, isEnd, timer, children }) => {
     return (
-        <div className={`imagen flex h-[250px] md:h-[100%] ${isPlaying ? 'invert-color' : ''}`} onClick={onClick}>
+        <button title="Reproducir música" className={`imagen flex h-[250px] md:h-[100%] ${isPlaying ? 'invert-color' : ''}`} onClick={onClick}>
             {children}
             <span className={`ml-auto mr-[5px] ${isEnd ? 'hidden' : ''}`} >{timer} s</span>
-        </div>
+        </button>
     );
 };
 
