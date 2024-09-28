0bcda6e246aacc3b37f42902d8aa372c986f2af0
Josecarlos Vidal <Josecarlosvidal2@gmail.com>
2024-09-27 22:43:28 -0300

    del logs

--- public/js/main.js
+++ public/js/main.js
@@ -1,10 +1,3 @@
 if ("serviceWorker" in navigator) {
-  navigator.serviceWorker
-    .register("/js/sw.js")
-    .then((registration) => {
-      console.log("Service Worker registrado con éxito:", registration);
-    })
-    .catch((error) => {
-      console.log("Error al registrar el Service Worker:", error);
-    });
+  navigator.serviceWorker.register("/js/sw.js");
 }
