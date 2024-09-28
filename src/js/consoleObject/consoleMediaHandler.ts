260bac1fc710ed07c213ade8526764d22fcf6646
Josecarlos Vidal <Josecarlosvidal2@gmail.com>
2024-09-28 13:52:15 -0300

    add support for pause/play button

--- src/js/consoleObject/consoleMediaHandler.ts
+++ src/js/consoleObject/consoleMediaHandler.ts
@@ -1,55 +1,55 @@
 export class ConsoleMediaHandler {
-    constructor() {
+  constructor() {
       this.setupAudioEvents();
-    }
-  
-    setupAudioEvents() {
+  }
+
+  setupAudioEvents() {
       const $rep = document.querySelector("#audio-rep") as HTMLAudioElement;
       if ($rep) {
-        $rep.addEventListener("ended", this.handleAudioEnd.bind(this));
+          $rep.addEventListener("ended", this.handleAudioEnd.bind(this));
+          $rep.addEventListener("play", this.playAudio.bind(this));
+          $rep.addEventListener("pause", this.pauseAudio.bind(this));
       }
-    }
-  
-    handleAudioEnd() {
+  }
+
+  handleAudioEnd() {
       const $thumbnail = document.querySelector(".imagen") as HTMLElement;
       $thumbnail.classList.remove("invert-color");
       const $timer = $thumbnail.querySelector("#timer") as HTMLElement;
-      $thumbnail.title =  "Reproducir música"; 
+      $thumbnail.title = "Reproducir música";
       const $volume = document.querySelector("#volume") as HTMLInputElement;
 
-      if($volume){
-        $volume.classList.add("opacity-0")
+      if ($volume) {
+          $volume.classList.add("opacity-0");
       }
-      
-      
+
       if ($timer) {
-        $timer.classList.add("opacity-0");
+          $timer.classList.add("opacity-0");
       }
-    }
-  
-    playAudio() {
+  }
+
+  playAudio() {
       const $thumbnail = document.querySelector(".imagen") as HTMLElement;
       const $rep = document.querySelector("#audio-rep") as HTMLAudioElement;
       const $timer = $thumbnail.querySelector("#timer") as HTMLElement;
       const $volume = document.querySelector("#volume") as HTMLInputElement;
 
-      $volume.classList.remove("opacity-0")
-  
+      $volume.classList.remove("opacity-0");
+
       $thumbnail.classList.add("invert-color");
       $timer?.classList.remove("opacity-0");
       $thumbnail.title = "Pausar música";
 
       $rep.play();
-    }
-  
-    pauseAudio() {
+  }
+
+  pauseAudio() {
       const $thumbnail = document.querySelector(".imagen") as HTMLElement;
       const $rep = document.querySelector("#audio-rep") as HTMLAudioElement;
-  
+
       $thumbnail.classList.remove("invert-color");
-      $thumbnail.title =  "Reproducir música";
-      
+      $thumbnail.title = "Reproducir música";
+
       $rep.pause();
-    }
   }
-  
+}
