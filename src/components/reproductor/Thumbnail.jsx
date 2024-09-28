a32af1a042b7acaba9dbf8992edbe923cba45708
Josecarlos Vidal <Josecarlosvidal2@gmail.com>
2024-09-27 22:19:14 -0300

    Add warning for dispos different to pc

--- src/components/reproductor/Thumbnail.jsx
+++ src/components/reproductor/Thumbnail.jsx
@@ -2,7 +2,7 @@ import "../../styles/imagenStyles.css";
 
 const Thumbnail = ({ onClick, isPlaying, isEnd, timer, children }) => {
     return (
-        <div className={`imagen flex ${isPlaying ? 'invert-color' : ''}`} onClick={onClick}>
+        <div className={`imagen flex h-[250px] md:h-[100%] ${isPlaying ? 'invert-color' : ''}`} onClick={onClick}>
             {children}
             <span className={`ml-auto mr-[5px] ${isEnd ? 'hidden' : ''}`} >{timer} s</span>
         </div>
