9e2f766765ec338ab2c6d6b2f42cc36d90557dec
Josecarlos Vidal <Josecarlosvidal2@gmail.com>
2024-09-28 13:32:31 -0300

    change hidden to opacity

--- src/components/reproductor/Thumbnail.jsx
+++ src/components/reproductor/Thumbnail.jsx
@@ -1,11 +1,25 @@
 import "../../styles/imagenStyles.css";
+import { Volume } from "./Volume";
 
-const Thumbnail = ({ onClick, isPlaying, isEnd, timer, children }) => {
+const Thumbnail = ({ audioRef, onClick, isPlaying, isEnd, timer, children }) => {
     return (
-        <button title="Reproducir música" className={`imagen flex h-[250px] md:h-[100%] ${isPlaying ? 'invert-color' : ''}`} onClick={onClick}>
-            {children}
-            <span className={`ml-auto mr-[5px] ${isEnd ? 'hidden' : ''}`} >{timer} s</span>
-        </button>
+<>
+<button 
+    title="Reproducir música" 
+    className={`imagen flex h-[250px] md:h-[100%] ${isPlaying ? 'invert-color' : ''} `} 
+    onClick={onClick}
+>
+    {children}
+    <div className="ml-auto mr-[5px] flex items-center justify-between flex-col h-full pb-3">
+    <span id="timer" className={`${isEnd ? 'opacity-0' : ''}`}>{timer} s</span>
+    </div>
+</button>
+<Volume 
+    audioRef={audioRef} 
+    className={"opacity-100 transition-all"} 
+    />
+</>
+
     );
 };
 
