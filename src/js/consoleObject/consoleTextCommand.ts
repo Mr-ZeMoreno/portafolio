6f4a7ec5b59de6c4ad9104ccd1109e21c4d0bd27
Josecarlos Vidal <Josecarlosvidal2@gmail.com>
2024-09-27 23:54:03 -0300

    Add framework svg to about command

--- src/js/consoleObject/consoleTextCommand.ts
+++ src/js/consoleObject/consoleTextCommand.ts
@@ -2,45 +2,101 @@ import { ConsoleMessage } from "./consoleMessage";
 import { SocialLinks } from "./socialLinks";
 
 export class ConsoleCommands {
-    padre: HTMLElement;
+  padre: HTMLElement;
 
-    constructor(padre: HTMLElement) {
-        this.padre = padre;
-    }
+  constructor(padre: HTMLElement) {
+    this.padre = padre;
+  }
 
-    hola() {
-        this.appendMessage(
-            "Hola, Soy Josecarlos, también conocido como Zemoreno. " +
-            "Soy apasionado de la programación 👨🏽‍💻, el sushi 🍣 y la música soul 🎧. " +
-            "No dudes en contactarme y disfruta de la canción de turno, " +
-            "la cual voy cambiando semanalmente.",
-            "middle-msg"
-        );
-        this.appendMessage(".", "middle-msg");
-        this.appendMessage(".", "middle-msg");
-    }
+  hola() {
+    this.appendMessage(
+      "Hola, Soy Josecarlos, también conocido como Zemoreno. " +
+        "Soy apasionado de la programación 👨🏽‍💻, el sushi 🍣 y la música soul 🎧. " +
+        "No dudes en contactarme y disfruta de la canción de turno, " +
+        "la cual voy cambiando semanalmente.",
+      "middle-msg"
+    );
+    this.appendMessage(".", "middle-msg");
+    this.appendMessage(".", "middle-msg");
+  }
 
-    help(comandos: { [key: string]: (inputValue: string) => void }) {
-        this.appendMessage("Comandos disponibles: " + Object.keys(comandos).join(", "), "middle-msg");
-    }
+  help(comandos: { [key: string]: (inputValue: string) => void }) {
+    this.appendMessage(
+      "Comandos disponibles: " + Object.keys(comandos).join(", "),
+      "middle-msg"
+    );
+  }
 
-    about(){
-        this.appendMessage("Esta página no fue creada como portafolio, si no más bien como mi espacio en el mundo.", "middle-msg")
-    }
-    contact() {
-        this.appendMessage("Puedes encontrarme en las siguientes redes sociales:", "middle-msg");
+  about() {
+    this.appendMessage(
+      "Esta página no fue creada como portafolio, " +
+        "si no más bien como mi espacio personal en el mundo.",
+      "middle-msg"
+    );
+    this.appendMessage(
+      ".",
+      "middle-msg"
+    );
+    this.appendMessage(
+      "Las herramientas usadas para este proyecto fueron:",
+      "middle-msg"
+    );
+    const $contenedorFrameworks = document.createElement("div");
 
-        const $contenedorRrss = document.createElement("div");
-        const socialLinks = new SocialLinks($contenedorRrss);
+    $contenedorFrameworks.style.marginTop = "5px";
+    $contenedorFrameworks.style.display = "flex";
+    $contenedorFrameworks.style.alignItems = "center";
+    $contenedorFrameworks.style.justifyContent = "center";
 
-        socialLinks.appendRrss("https://www.linkedin.com/in/josecarlos-vidal/", "/svg/linkedin.svg", "LinkedIn");
-        socialLinks.appendRrss("https://github.com/Mr-ZeMoreno", "/svg/github.svg", "GitHub");
+    const socialLinks = new SocialLinks($contenedorFrameworks);
 
-        this.padre.appendChild($contenedorRrss);
-    }
+    socialLinks.appendRrss(
+      "https://astro.build/",
+      "/svg/astro.svg",
+      "Astro"
+    );
+    socialLinks.appendRrss(
+      "https://es.react.dev/",
+      "/svg/react.svg",
+      "React"
+    );
+    socialLinks.appendRrss(
+      "https://tailwindcss.com/",
+      "/svg/tailwind.svg",
+      "Tailwind"
+    );
 
-    appendMessage(inputValue: string, clase: string) {
-        const newMessage = new ConsoleMessage(this.padre, inputValue, clase);
-        newMessage.displayMessage();
-    }
+    this.padre.appendChild($contenedorFrameworks);
+  }
+  contact() {
+    this.appendMessage(
+      "Puedes encontrarme en las siguientes redes sociales:",
+      "middle-msg"
+    );
+
+    const $contenedorRrss = document.createElement("div");
+    $contenedorRrss.style.marginTop = "5px";
+    $contenedorRrss.style.display = "flex";
+    $contenedorRrss.style.alignItems = "center";
+    $contenedorRrss.style.justifyContent = "center";
+    const socialLinks = new SocialLinks($contenedorRrss);
+
+    socialLinks.appendRrss(
+      "https://www.linkedin.com/in/josecarlos-vidal/",
+      "/svg/linkedin.svg",
+      "LinkedIn"
+    );
+    socialLinks.appendRrss(
+      "https://github.com/Mr-ZeMoreno",
+      "/svg/github.svg",
+      "GitHub"
+    );
+
+    this.padre.appendChild($contenedorRrss);
+  }
+
+  appendMessage(inputValue: string, clase: string) {
+    const newMessage = new ConsoleMessage(this.padre, inputValue, clase);
+    newMessage.displayMessage();
+  }
 }
