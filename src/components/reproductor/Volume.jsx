927a03674c993b06ab65ce8505b9e06d57a6ed42
Josecarlos Vidal <Josecarlosvidal2@gmail.com>
2024-09-28 13:33:35 -0300

    Volume component

---
+++ src/components/reproductor/Volume.jsx
@@ -0,0 +1,27 @@
+import "../../styles/volumeStyles.css"
+import { useState } from 'react';
+
+export function Volume({audioRef, className}){
+    const [volume, setVolume] = useState(1); 
+
+    const handleVolumeChange = (e) => {
+        const newVolume = e.target.value;
+        setVolume(newVolume);
+        if (audioRef.current) {
+            audioRef.current.volume = newVolume;
+        }
+    };
+    return (
+        <>
+            <input className={`volume ${className}`} 
+                type="range"
+                min="0"
+                max="1"
+                step="0.01"
+                id="progressBar"
+                value={volume}
+                onChange={handleVolumeChange}
+            />
+        </>
+    )
+}
