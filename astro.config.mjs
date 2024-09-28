94899362c66cc89396e2dd5d7bd56ad75cda992f
Josecarlos Vidal <Josecarlosvidal2@gmail.com>
2024-09-27 22:39:09 -0300

    Add prod env

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,21 @@
-import { defineConfig } from "astro/config";
+import { defineConfig, envField } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import vercel from "@astrojs/vercel/serverless";
 import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
+  experimental: {
+    env: {
+      schema: {
+        PROD_ENV: envField.boolean({
+          context: "client",
+          access: "public",
+          optional: false,
+        }),
+      },
+    },
+  },
   integrations: [tailwind(), react()],
   devToolbar: {
     enabled: false,
--- src/env.d.ts
+++ src/env.d.ts
@@ -1,9 +1,1 @@
 /// <reference path="../.astro/types.d.ts" />
-// src/env.d.ts
-interface ImportMetaEnv {
-    VITE_API_KEY: string;
-}
-
-interface ImportMeta {
-    readonly env: ImportMetaEnv;
-}
