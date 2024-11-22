e3d86223b212728fb560aeedf0c16beb9e60ee16
Josecarlos Vidal <Josecarlosvidal2@gmail.com>
2024-11-21 23:02:47 -0300

    chore: actualizar a 5.0 de astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,21 +1,10 @@
 import { defineConfig, envField } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-import vercel from "@astrojs/vercel/serverless";
+import vercel from "@astrojs/vercel";
 import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  experimental: {
-    env: {
-      schema: {
-        PROD_ENV: envField.boolean({
-          context: "client",
-          access: "public",
-          optional: false,
-        }),
-      },
-    },
-  },
   integrations: [tailwind(), react()],
   devToolbar: {
     enabled: false,
